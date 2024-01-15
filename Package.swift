cdf5c17fa48d1e924447693dd5133d186eea5556
Oleksandr H <fluder@icloud.com>
2024-01-15 02:19:54 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "xczip",
     platforms: [.macOS(.v10_12)],
     dependencies: [
-        .package(url: "https://github.com/DimaRU/ZIPFoundation", .branch("forcedate")),
+        .package(url: "https://github.com/glushchenko/ZIPFoundation", .branch("forcedate")),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
